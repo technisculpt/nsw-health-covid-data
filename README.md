084f4901a56d3e2ac5883b9c7b7270fef3f9ba50
mark <marcolinilagana@gmail.com>
2023-06-09 11:53:55 +1000

    20230609

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ aggregate data from different sources into easy to access .csv files
 
 death information from the weekly PDF was canned so working on scraping it from the website
 
-![alt text](https://github.com/technisculpt/nsw-health-covid-data/blob/main/20220120-20230606.png)
+![alt text](https://github.com/technisculpt/nsw-health-covid-data/blob/main/20220120-20230609.png)
 
-![alt text](https://github.com/technisculpt/nsw-health-covid-data/blob/main/20220120-20230606_log.png)
+![alt text](https://github.com/technisculpt/nsw-health-covid-data/blob/main/20220120-20230609_log.png)
--- nsw_website.py
+++ nsw_website.py
@@ -5,9 +5,6 @@ from dateutil.parser import parse
 import json
 import os
 
-# polling website to extracting People in hospital, People in ICU, and lives lost in the past 7 days
-# it has worked but is tempermental, work in progress
-
 url = r'https://www.health.nsw.gov.au/Infectious/covid-19/Pages/stats-nsw.aspx'
 
 try:
@@ -22,46 +19,49 @@ except requests.exceptions.RequestException as e:
     print('Failed to retrieve. Error:', str(e))
     exit()
 
-
 soup = BeautifulSoup(response.content, 'html.parser')
 
-try:
-    script_tag = soup.find('script', text=re.compile('clientServerTimeDelta'))
-    date_string = re.search(r'new Date\\(\"(.*?)\"\\)', script_tag.string).group(1)
-    date = parse(date_string)
-except Exception as e:
-    print('Failed to extract content. Error:', str(e))
-    exit()
+# try:
+date_tag = soup.find('a', {'id': 'active'})
+for child in date_tag: 
+    print(child.text) 
 
-if os.path.exists('date.json'):
-    with open('date.json', 'r') as f:
-        stored_date = json.load(f).get('stats-nsw')
+#     date_string = re.search(r'up to 4pm (.*?)$', date_tag.string).group(1)
+#     print(date_string)
+#     date = parse(date_string)
+# except Exception as e:
+#     print('Failed to extract content. Error:', str(e))
+#     exit()
 
-    if str(date) == stored_date:
-        print('No new cases.')
-        exit()
+# if os.path.exists('date.json'):
+#     with open('date.json', 'r') as f:
+#         stored_date = json.load(f).get('stats-nsw')
 
-try:
-    active_cases_div = soup.find('div', {'class': 'active-cases calloutbox'})
-    numbers = active_cases_div.find_all('span', {'class': 'number'})
-    hospitalized = numbers[0].text
-    icu = numbers[1].text
-    lives_lost = numbers[2].text
-    hospitalized = int(hospitalized.replace(',', ''))
-    icu = int(icu.replace(',', ''))
-    lives_lost = int(lives_lost.replace(',', ''))
+#     if str(date) == stored_date:
+#         print('No new cases.')
+#         exit()
 
-except Exception as e:
-    print('Failed to extract health data. Error:', str(e))
-    exit()
+# try:
+#     active_cases_div = soup.find('div', {'class': 'active-cases calloutbox'})
+#     numbers = active_cases_div.find_all('span', {'class': 'number'})
+#     hospitalized = numbers[0].text
+#     icu = numbers[1].text
+#     lives_lost = numbers[2].text
+#     hospitalized = int(hospitalized.replace(',', ''))
+#     icu = int(icu.replace(',', ''))
+#     lives_lost = int(lives_lost.replace(',', ''))
 
-print(f"{date = }")
-print(f"{hospitalized = }")
-print(f"{icu = }")
-print(f"{lives_lost = }")
+# except Exception as e:
+#     print('Failed to extract health data. Error:', str(e))
+#     exit()
 
-with open('date.json', 'w') as f:
-    json.dump({'stats-nsw': str(date)}, f)
+# print(f"{date = }")
+# print(f"{hospitalized = }")
+# print(f"{icu = }")
+# print(f"{lives_lost = }")
 
-with open(f'{date}.html', 'w') as f:
-    f.write(response.content.decode())
+# with open('date.json', 'w') as f:
+#     json.dump({'stats-nsw': str(date)}, f)
+
+# with open(f'{date}.html', 'w') as f:
+#     f.write(response.content.decode())
